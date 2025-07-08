ddd7b762e0054353821d12a4c4b3c32d408cda84
ejmaller7 <mallerethan4@gmail.com>
2025-07-08 16:46:58 -0500

    Connecting MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+// Imports here - different from what you initially learned but I believe this is the older version
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Connect DB
+connectDB();
 
 app.get('/', (req, res) => res.send('API running'));
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
