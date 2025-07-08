44ad784796ab95bef1273cd873a3a634f3bb72bd
ejmaller7 <mallerethan4@gmail.com>
2025-07-08 16:55:51 -0500

    Temporary pause from instructional videos to created index.js file & use that for all routes (more commits to follow)

---
+++ routes/api/index.js
@@ -0,0 +1,5 @@
+import Users from './users';
+import Auth from './auth';
+import Profile from './profile';
+import Posts from './posts';
+
--- server.js
+++ server.js
@@ -2,11 +2,17 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+// Import the way you know how to here
+import Routes from './routes/index.js';
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Connect DB
 connectDB();
+
+// Define routes
+app.use(Routes);
 
 app.get('/', (req, res) => res.send('API running'));
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
