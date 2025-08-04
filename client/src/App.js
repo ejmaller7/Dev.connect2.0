49f83c4071cceb8772abec3d13ddf670a2d841ec
ejmaller7 <mallerethan4@gmail.com>
2025-08-04 14:43:57 -0500

    Adding 2 more components Register & Login. . Configuring the routes in App.js, Navbar.js & Landing.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import './App.css';
 
 const App = () =>
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' Component={Landing} />
+        </Routes>
+        <section className='container'>
+          <Routes>
+            <Route exact path='/register' Component={Register} />
+            <Route exact path='/login' Component={Login} />
+          </Routes>
+        </section>
+      </Fragment>
+    </Router>
 
 export default App;
