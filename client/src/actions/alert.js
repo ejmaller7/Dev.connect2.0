c4e58b7d76e722a1c0ccc4ffae580cb9caa455e4
ejmaller7 <mallerethan4@gmail.com>
2025-08-05 12:17:16 -0500

    Changing the timeout default to 5 seconds for removing an alert

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from "./types";
 
-export const setAlert = (msg, alertType) => dispatch => {
+export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
     const id = uuidv4();
     
     // Displays the alert message, type of alert and the ID
@@ -11,5 +11,5 @@ export const setAlert = (msg, alertType) => dispatch => {
     });
 
     // Removes the alert after 5 seconds
-    setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), 5000);
+    setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
 };
