6261c36dc9d61748acc05f52200bb19e152b4d09
ejmaller7 <mallerethan4@gmail.com>
2025-08-11 11:45:00 -0500

    Adding in an action to submit the profile and navigate back to the dashboard on submitting the form

--- client/src/components/profile-forms/CreateProfile.jsx
+++ client/src/components/profile-forms/CreateProfile.jsx
@@ -1,8 +1,11 @@
 import React, { Fragment, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { createProfile } from '../../actions/profile';
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
+    const navigate = useNavigate();
     const [formData, setFormData] = useState({
         company: '',
         website: '',
@@ -23,7 +26,13 @@ const CreateProfile = props => {
     // Destructuring the formData so we can access all of these fields easily
     const { company, website, location, status, skills, githubusername, bio, twitter, facebook, linkedin, youtube, instagram } = formData;
 
-    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+    const onChange = e => 
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = e => {
+        e.preventDefault();
+        createProfile(formData, navigate);
+    }
 
     return (
     <Fragment>
@@ -35,7 +44,7 @@ const CreateProfile = props => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" on onSubmit={e => onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={status} onChange={e => onChange(e)}>
             <option value="0">* Select Professional Status</option>
@@ -139,7 +148,7 @@ const CreateProfile = props => {
 }
 
 CreateProfile.propTypes = {
+    createProfile: PropTypes.func.isRequired,
+};
 
-}
-
-export default CreateProfile
+export default connect(null, { createProfile })(CreateProfile);
