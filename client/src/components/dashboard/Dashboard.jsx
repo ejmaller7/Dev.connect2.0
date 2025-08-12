1405d815deca46b5ae19db9ced41f74d1d023542
ejmaller7 <mallerethan4@gmail.com>
2025-08-12 16:59:55 -0500

    Adding Experiences to the dashboard (Commit 1 of 2) - incomplete

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -6,6 +6,7 @@ import { getCurrentProfile } from '../../actions/profile';
 // import auth from '../../reducers/auth';
 import Spinner from '../layout/Spinner';
 import DashboardActions from './DashboardActions';
+import Experience from './Experience';
 
 const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
   useEffect(() => {
@@ -22,6 +23,7 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
       {profile !== null ? (
       <Fragment>
         <DashboardActions />
+        <Experience experience={profile.experience} />
       </Fragment> 
       ) : ( 
       <Fragment>
