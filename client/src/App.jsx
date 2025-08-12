9c4bdfef505b24cf3b310c1104b1b61b1e28493e
ejmaller7 <mallerethan4@gmail.com>
2025-08-12 16:15:52 -0500

    Adding actions and reducers for adding experiences and educations into the profile. Creating 2 jsx files for adding education & experience.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,8 @@ import PrivateRoute from './components/routing/PrivateRoute';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 import './App.css';
 
 // Redux
@@ -44,6 +46,8 @@ const App = () => {
                 <Route path='/dashboard' element={<Dashboard />} />
                 <Route path='/create-profile' element={<CreateProfile />} />
                 <Route path='/edit-profile' element={<EditProfile />} />
+                <Route path='/add-experience' element={<AddExperience />} />
+                <Route path='/add-education' element={<AddEducation />} />
               </Route>
             </Routes>
           </section>
--- client/src/components/profile-forms/CreateProfile.jsx
+++ client/src/components/profile-forms/CreateProfile.jsx
@@ -44,7 +44,7 @@ const CreateProfile = ({ createProfile }) => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form" on onSubmit={e => onSubmit(e)}>
+      <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={status} onChange={e => onChange(e)}>
             <option value="0">* Select Professional Status</option>
