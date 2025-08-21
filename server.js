c455312a4c51e15eb355002067617433433c96e6
ejmaller7 <mallerethan4@gmail.com>
2025-08-21 16:27:51 -0500

    Attempt 6 for the day (Please work)

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if(process.env.NODE_ENV === 'default') {
     // Set static folder
     app.use(express.static(path.join('client/build')));
 
-    app.get('/(.*)', (req, res) => {
+    app.get('/*)', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     });
 }
