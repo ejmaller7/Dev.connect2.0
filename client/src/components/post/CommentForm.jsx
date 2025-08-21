ac50c50f83d16476c203bb23c0e4227dae244a1d
ejmaller7 <mallerethan4@gmail.com>
2025-08-21 15:07:42 -0500

    Adding a way to display comments under a post & fixing the useState on submit to clear the text once a post or comment is submitted

--- client/src/components/post/CommentForm.jsx
+++ client/src/components/post/CommentForm.jsx
@@ -6,18 +6,21 @@ import { addComment } from '../../actions/post';
 const CommentForm = ({ postId, addComment }) => {
     const [text, setText] = useState('');
 
+    const onSubmit = (e) => {
+        e.preventDefault();
+        addComment(postId, { text });
+        setText('');
+    }
+    
     return (
         <div className="post-form">
             <div className="bg-primary p">
                 <h3>Leave a Comment</h3>
             </div>
-            <form className="form my-1" onSubmit={e => {
-                e.preventDefault();
-                addComment(postId, { text });
-                setText('');
-            }}>
+            <form className="form my-1" onSubmit={onSubmit}>
                 <textarea
                     name="text"
+                    value={text}
                     cols="30"
                     rows="5"
                     placeholder="Create a comment"
