2d334b72931066fcf6dc2aa25942b3a00037ea25
ejmaller7 <mallerethan4@gmail.com>
2025-08-21 16:23:59 -0500

    Attempt 5 or something... Hopefully it deploys

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(routes);
 // Serve static assets in production
 if(process.env.NODE_ENV === 'default') {
     // Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join('client/build')));
 
     app.get('/(.*)', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
